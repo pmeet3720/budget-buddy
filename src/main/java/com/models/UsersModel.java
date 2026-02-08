803dff12b9e0df2a0b209273b448d44edd539030
Meet Patel <pmeet3720@gmail.com>
2026-02-08 18:47:56 +0530

    new folder added

--- src/main/java/com/models/UsersModel.java
+++ src/main/java/com/models/UsersModel.java
@@ -8,7 +8,7 @@ import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import com.Roles;
+import com.enums.Roles;
 
 import lombok.AccessLevel;
 import lombok.Data;
